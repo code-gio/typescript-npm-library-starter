a568a7aba7194d29fd413e331d7fdb513c0fd9d9
code-gio <138194389+code-gio@users.noreply.github.com>
2025-04-04 10:37:36 -0600

    removed incorrect file

--- src/core/module-registry.ts
+++
@@ -1,7 +0,0 @@
-import type { ModuleConfig } from './types';
-
-/**
- * Registry of all available SDK modules
- * Modules register themselves by adding to this array
- */
-export const moduleRegistry: ModuleConfig[] = [];
